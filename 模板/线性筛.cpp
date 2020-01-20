c8216c106459acddf555f0a9ed36d938d8f9b70b
limejuice <limejuice1536@hotmail.com>
2020-01-20 17:39:18 +0800

    修正并扩展线性筛模板

--- 模板/线性筛.cpp
+++ 模板/线性筛.cpp
@@ -1,29 +1,47 @@
-#include<bits/stdc++.h>
-#define N 10005
-using namespace std;
+#include <vector>
+#include <algorithm>
+
 bool prime[N];
+int divcnt[N];
+int mindiv[N],mindivcnt[N]; // minimum prime divisor of i
+int phi[N];
+//...
 
 void sieve(int n)
 {
 	vector<int> primelist;
-	fill(prime,prime+n,true);
-	prime[1]=false;
-	for(int i=2;i<n;i++) {
-		if(prime[i]) primelist.push_back(i);
-		for(vector<int>::iterator v=primelist.begin();v!=primelist.end();v++) {
-			if(i/(*v)==0||i*(*v)>n) break;
-			prime[i*(*v)]=false;
+	fill(prime,prime + n,true);
+	prime[1] = false;
+	mindiv[1] = 1;
+	for(int i = 2;i < n;i++) {
+		if(prime[i]) {
+			primelist.push_back(i);
+			mindiv[i] = i;
+			mindivcnt[i] = divcnt[i] = 1;
+			phi[i] = i - 1;
+		}
+		for(vector<int>::iterator v = primelist.begin();v != primelist.end();v++) {
+			int p = *v, nxt = i * p;
+			if(nxt > n)
+				break;
+			mindiv[nxt] = p;
+			prime[nxt] = false;
+			divcnt[nxt] = divcnt[i] + 1;
+			if(i % p == 0) {
+				mindivcnt[nxt] = mindivcnt[i] + 1;
+				phi[nxt] = phi[i] * p; // phi[i] * nxt / i
+				break;
+			} else {
+				mindivcnt[nxt] = 1;
+				phi[nxt] = phi[i] * (p - 1); // phi[i] * nxt * (p - 1) / i / p
+			}
 		}
 	}
 }
 
 int main()
 {
-	int n;
-	cin>>n;
+	//...
 	sieve(n);
-	for(int i=1;i<n;i++) {
-		if(prime[i]) cout<<i<<endl;
-	}
-	return 0;
+	//...
 }
