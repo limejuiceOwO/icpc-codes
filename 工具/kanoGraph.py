cb0711eea8cc50d9d66395e71828cad0c73a2d9d
limejuice <limejuice1536@hotmail.com>
2020-04-08 17:57:25 +0800

    升级卡诺图生成器：简化函数表示，增加无关项和列表转换功能

--- 工具/kanoGraph.py
+++ 工具/kanoGraph.py
@@ -1,9 +1,3 @@
-class Wrapper:
-	def __init__(self,s):
-		self.s = s
-	def __getattr__(self,attr):
-		return self.s[attr[0].encode("ascii")[0] - b'a'[0]]
-
 def gray(n):
 	for x in range(0,2 ** n):
 		bitseq = [0] * n;
@@ -17,16 +11,39 @@ def r(x):
 	# Acts as !X
 	return x ^ 1
 
-def kanoGraph(n,f):
+def kanoGraph(n,f,irrelevant=None):
 	''' Prints the n-bit Karnaugh map of boolean function f.
+		Prints 'x' when irrelevant(a,b,c...) returns true.
 		For example:
-			>> kanoGraph(4,lambda s:(s.a | s.b) & (s.a | s.b | s.c) & (r(s.a) | s.c) & (s.b | s.c | s.d))
-			prints out a 4-bit map of function (A+B)(A+B+C)(!A + C)(B+C+D)
+			>> kanoGraph(4,lambda a,b,c,d:r(a | b) & (a | b | c) & (r(a) | c) & (b | c | d),lambda a,b,c,d:b == c)
+			prints out a 4-bit map of function !(A+B)(A+B+C)(!A + C)(B+C+D),with values showed as 'x' when B = C,like:
+				x 0 0 x
+				x 0 0 x
+				1 x x 0
+				1 x x 0
 	'''
 	ln = n // 2
 	col = n - ln
 	for s1 in gray(ln):
 		for s2 in gray(col):
-			s = s2 + s1
-			print(int(f(Wrapper(s))),end = ' ')
+			kwargs = {}
+			for i,v in enumerate(s2 + s1):
+				kwargs[chr(ord('a') + i)] = v
+			if(not (irrelevant and irrelevant(**kwargs))):
+				print(int(f(**kwargs)),end = ' ')
+			else:
+				print('x',end = ' ')
+		print()
+
+def kanoGraph2(n,l):
+	''' Prints the n-bit Karnaugh map of a value list l of length 2^n.
+	'''
+	ln = n // 2
+	col = n - ln
+	for s1 in gray(ln):
+		for s2 in gray(col):
+			p = 0
+			for i,b in enumerate(s2 + s1):
+				p += b * (1 << (n - i - 1))
+			print(l[p],end = ' ')
 		print()
